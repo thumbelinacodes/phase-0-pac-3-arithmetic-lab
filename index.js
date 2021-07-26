89a44e7a4613d0835175a32073f080c4caea9d5e
Quintia <quintialy@gmail.com>
2021-07-25 23:55:03 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(number1,number2) {
+    return number1+number2;
+}
+function subtract(number2, number1) {
+    return number2-number1;
+}
+function multiply(number1, number2) {
+    return number1* number2;
+}
+function divide(number1, number2){
+    return (number1/ number2);
+}
+function increment (n) {
+    return n+ 1;
+}
+function decrement (n){
+    return n -1;
+}
+function makeInt(n) {
+    parseInt (n, 10);
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    parseFloat (n, 10);
+    return parseFloat (n, 10);
+}
